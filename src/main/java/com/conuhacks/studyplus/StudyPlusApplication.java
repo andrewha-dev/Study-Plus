b85263332c8b4312c56d2a58a214d8c58e527039
ha-andrew <34655105+ha-andrew@users.noreply.github.com>
2019-01-24 15:57:35 -0500

    First Commit

---
+++ src/main/java/com/conuhacks/studyplus/StudyPlusApplication.java
@@ -0,0 +1,14 @@
+package com.conuhacks.studyplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlusApplication.class, args);
+	}
+
+}
+
