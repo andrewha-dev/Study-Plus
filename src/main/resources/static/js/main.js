50d3eb7f8b9e296dce198b5efb7cb9c9d7cd54fc
ha-andrew <34655105+ha-andrew@users.noreply.github.com>
2019-01-26 18:58:51 -0500

    using js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,5 +2,36 @@ $(document).ready(function(){
 	
 	console.log('Hello World');
 	
+	const campus = ["Sir George Williams", "Loyola"]
+	const sgwBuildings = ["FG", "Hall", "JMSB"]
+	const loyBuildings = ["CJ", "FC", "PY", "SP", "VE"]
+	console.log(campus);
+	
+	$("#campus").change(function(e){
+		console.log(this.value);
+		$('#building')
+	    .empty()
+		if (this.value == "loy")
+		{
+			$.each(loyBuildings, function(key, value){
+				console.log("Key: " + key)
+				console.log("Val: " + value)	
+				$("#building").append("<option value='" + value + "'>" + value + " Building" + "</option>");
+			})
+		}		
+		else
+		{
+			$.each(sgwBuildings, function(key, value){
+				console.log("Key: " + key)
+				console.log("Val: " + value)	
+				$("#building").append("<option value='" + value + "'>" + value + " Building" + "</option>");
+			})
+		}
+		
+	});
+	
+	
+	
+	
 	
 });
